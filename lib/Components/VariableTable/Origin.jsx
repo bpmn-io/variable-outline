3130c9e3674c25c3b3f7befad054e79a4456331f
Maciej Barelkowski <maciej.barelkowski@camunda.com>
2025-03-28 11:50:50 +0100

    fix: explicitly handle first two origins
    Related to https://github.com/camunda/camunda-modeler/issues/4940

--- lib/Components/VariableTable/Origin.jsx
+++ lib/Components/VariableTable/Origin.jsx
@@ -6,15 +6,18 @@ import './Origin.scss';
 export default function Origin(props) {
   const { origins } = props;
 
-  const firstTwoOrigins = origins.slice(0, 2);
+  const [ firstOrigin, secondOrigin ] = origins.slice(0, 2);
   const additionalOrigins = origins.length - 2;
 
   return <div className="bio-vo-origin">
     {
-      firstTwoOrigins
-        .map((origin, index) => <>
-          { index ? ' | ' : null } <Element key={ origin.id } element={ origin } />
-        </>)
+      <Element element={ firstOrigin } />
+    }
+    {
+      secondOrigin ? <>
+        { ' | ' }
+        <Element element={ secondOrigin } />
+      </> : null
     }
     {additionalOrigins > 0 && <Tag type="gray">{`+${additionalOrigins}`}</Tag>}
   </div>;
