99bd1fe0ab0cc7fb6e751ac710748a67957cc81c
Ali Barin <ali.barin@camunda.com>
2026-02-22 22:46:46 +0100

    feat: add info tooltip button by search bar

--- lib/Components/Search/Search.jsx
+++ lib/Components/Search/Search.jsx
@@ -1,4 +1,5 @@
-import { Search } from '@carbon/react';
+import { Search, Tooltip } from '@carbon/react';
+import { Help } from '@carbon/icons-react';
 
 import { FilterContext } from '../../Context/FilterContext';
 import { useContext } from 'react';
@@ -27,6 +28,13 @@ export default () => {
       value={ search }
       type="inline"
     />
+    <Tooltip
+      className="bio-vo-help"
+      label="This panel shows the variables accessible to the selected element, grouped by scope. Expand a variable to see which elements write it and what value it holds. Use the search or filters to narrow down the list."
+      align="bottom-end"
+    >
+      <Help />
+    </Tooltip>
   </div>;
 
 };
