a6f53e87920275e4da803b5d7c8cd4bc73186fd7
Ali Barin <ali.barin@camunda.com>
2026-03-02 16:13:09 +0100

    refactor: extract navigation logic to useElementNavigation hook

--- lib/Components/VariableList/ElementEntry.jsx
+++ lib/Components/VariableList/ElementEntry.jsx
@@ -1,40 +1,13 @@
-import { useContext } from 'react';
-import { is } from 'bpmn-js/lib/util/ModelUtil';
-import useService from '../../hooks/useService';
+import useElementNavigation from '../../hooks/useElementNavigation';
 import { getName } from '../../utils/elementUtil';
-import { FilterContext } from '../../Context/FilterContext';
 
 export default function ElementEntry({ element: bo, inline = false }) {
-
-  const [ , setFilter ] = useContext(FilterContext);
-  const selection = useService('selection');
-  const canvas = useService('canvas');
-  const elementRegistry = useService('elementRegistry');
-
-  const selectedElements = selection.get();
-  const isSelected = selectedElements.some(el => el.id === bo.id);
-
-  const handleClick = () => {
-    if (is(bo, 'bpmn:Process')) {
-      selection.select([]);
-      setFilter(prev => ({ ...prev, selectedElements: [] }));
-      return;
-    }
-
-    const element = elementRegistry.get(bo.id);
-
-    if (!element) {
-      return;
-    }
-
-    canvas.scrollToElement(element);
-    selection.select(element);
-  };
+  const { isSelected, navigate } = useElementNavigation(bo);
 
   return (
     <button
       className={ `variable-element-entry${inline ? ' variable-element-entry--inline' : ''}${isSelected ? ' variable-element-entry--selected' : ''}` }
-      onClick={ handleClick }
+      onClick={ navigate }
     >
       { getName(bo) }
     </button>
