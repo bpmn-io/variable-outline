c15ffc9e5037f1f15b27de414572b27fd5d88fe8
Ali BARIN <1263419+barinali@users.noreply.github.com>
2026-03-16 17:23:01 +0100

    feat: vertically center empty state and add learn more link (#57)
    Closes #56

--- lib/index.jsx
+++ lib/index.jsx
@@ -7,19 +7,21 @@ import { ScopeExpandProvider } from './context/ScopeExpandContext';
  *
  * @param {Object} props
  * @param {*} props.injector
+ * @param {string} [props.learnMoreUrl]
  * @returns {import('react').ReactElement}
  */
 export default function VariableOutline(props) {
 
   const {
-    injector
+    injector,
+    learnMoreUrl
   } = props;
 
   return (
     <InjectorContext.Provider value={ injector }>
       <FilterProvider>
         <ScopeExpandProvider>
-          <TabContent />
+          <TabContent learnMoreUrl={ learnMoreUrl } />
         </ScopeExpandProvider>
       </FilterProvider>
     </InjectorContext.Provider>
