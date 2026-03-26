6d930f390b4e1e94dddf86a70e89ba9f0f24b309
Ali Barin <ali.barin@camunda.com>
2026-03-26 22:59:27 +0300

    feat: update color and typography
    Closes camunda/camunda-modeler#5800

--- lib/components/CopyButton.jsx
+++ lib/components/CopyButton.jsx
@@ -1,4 +1,5 @@
 import { Copy, Checkmark } from '@carbon/icons-react';
+import { Tooltip } from '@carbon/react';
 import useClipboardCopy from '../hooks/useClipboardCopy';
 import useTracking from '../hooks/useTracking';
 
@@ -12,21 +13,22 @@ export default function CopyButton({ text }) {
   };
 
   return (
-    <button
-      className={ `variable-copy-button${ copied ? ' variable-copy-button--copied' : '' }` }
-      type="button"
-      title="Copy variable name"
-      aria-label="Copy variable name"
-      onClick={ handleClick }
-    >
-      <span aria-live="polite" style={ { display: 'none' } }>
-        { copied ? 'Copied to clipboard!' : '' }
-      </span>
+    <Tooltip label="Copy variable name" align="left" autoAlign>
+      <button
+        className={ `variable-copy-button${ copied ? ' variable-copy-button--copied' : '' }` }
+        type="button"
+        aria-label="Copy variable name"
+        onClick={ handleClick }
+      >
+        <span aria-live="polite" style={ { display: 'none' } }>
+          { copied ? 'Copied to clipboard!' : '' }
+        </span>
 
-      { copied
-        ? <Checkmark className="variable-copy-icon" />
-        : <Copy className="variable-copy-icon" />
-      }
-    </button>
+        { copied
+          ? <Checkmark className="variable-copy-icon" />
+          : <Copy className="variable-copy-icon" />
+        }
+      </button>
+    </Tooltip>
   );
 }
