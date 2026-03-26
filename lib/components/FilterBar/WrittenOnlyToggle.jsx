e91440ab7197267ad6d4f199cb936903fb414cc1
Ali Barin <ali.barin@camunda.com>
2026-03-26 18:22:11 +0300

    feat: add tooltip to written by selection toggle
    Wraps the WrittenOnlyToggle in a Carbon Tooltip to explain
    what the toggle does when hovered.
    
    Closes camunda/camunda-modeler#5785

--- lib/components/FilterBar/WrittenOnlyToggle.jsx
+++ lib/components/FilterBar/WrittenOnlyToggle.jsx
@@ -1,4 +1,4 @@
-import { Toggle } from '@carbon/react';
+import { Toggle, Tooltip } from '@carbon/react';
 import useFilter from '../../hooks/useFilter';
 
 export default function WrittenOnlyToggle() {
@@ -9,17 +9,23 @@ export default function WrittenOnlyToggle() {
   };
 
   return (
-    <div className="bio-vo-written-only-toggle">
-      <Toggle
-        id="written-only-toggle"
-        size="sm"
-        labelText="Written by selection"
-        hideLabel
-        toggled={ writtenOnly }
-        onToggle={ handleToggle }
-        aria-label="Written by current selection"
-      />
-    </div>
+    <Tooltip
+      label="Only show variables written by the currently selected element."
+      align="bottom"
+      autoAlign
+    >
+      <div className="bio-vo-written-only-toggle">
+        <Toggle
+          id="written-only-toggle"
+          size="sm"
+          labelText="Written by selection"
+          hideLabel
+          toggled={ writtenOnly }
+          onToggle={ handleToggle }
+          aria-label="Written by current selection"
+        />
+      </div>
+    </Tooltip>
   );
 
 }
